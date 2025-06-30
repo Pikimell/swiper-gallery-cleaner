bb935f46ff2a994a620900a9b81e60c62b8a5264
Volodymyr Pashchenko <volodkaposhta@gmail.com>
2025-06-30 14:50:54 +0300

    Initial Commit

---
+++ gallery-cleaner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gallery-cleaner
+//
+//  Created by Володимир Пащенко on 30.06.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gallery-cleaner/gallery_cleanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  gallery_cleanerApp.swift
+//  gallery-cleaner
+//
+//  Created by Володимир Пащенко on 30.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct gallery_cleanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
